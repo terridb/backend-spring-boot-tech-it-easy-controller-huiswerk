4fabf585bfe632b9378028d9e4e651030be6835c
terridb <terrideboer99@gmail.com>
2025-09-16 17:41:08 +0200

    first commit

---
+++ TechItEasy/src/main/java/com/techiteasy/TechItEasy/TechItEasyApplication.java
@@ -0,0 +1,13 @@
+package com.techiteasy.TechItEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechItEasyApplication.class, args);
+	}
+
+}
---
+++ TechItEasy/src/test/java/com/techiteasy/TechItEasy/TechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techiteasy.TechItEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
