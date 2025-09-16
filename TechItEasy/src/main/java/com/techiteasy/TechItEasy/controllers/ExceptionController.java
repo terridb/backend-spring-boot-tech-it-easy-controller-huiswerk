5d25e3e33681ad3235a1923cedbbc7c1d042c711
terridb <terrideboer99@gmail.com>
2025-09-16 18:14:37 +0200

    ExceptionController en RecordNotFoundException toegevoegd

---
+++ TechItEasy/src/main/java/com/techiteasy/TechItEasy/controllers/ExceptionController.java
@@ -0,0 +1,16 @@
+package com.techiteasy.TechItEasy.controllers;
+
+import com.techiteasy.TechItEasy.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
