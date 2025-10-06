ed29c34dea3b144e0c09783b255ddae902758b35
terridb <terrideboer99@gmail.com>
2025-10-06 14:35:36 +0200

    Put & get by id in TelevisionService toegevoegd

--- src/main/java/com/techiteasy/TechItEasy/controllers/TelevisionController.java
+++ src/main/java/com/techiteasy/TechItEasy/controllers/TelevisionController.java
@@ -31,55 +31,25 @@ public class TelevisionController {
         return ResponseEntity.ok(this.service.getAllTelevisions());
     }
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Television> getTelevisionById(@PathVariable Long id) {
-//        Optional<Television> ot = this.repos.findById(id);
-//
-//        if (ot.isPresent()) {
-//            return ResponseEntity.ok(ot.get());
-//        } else {
-//            throw new RecordNotFoundException("Television with id " + id + " not found");
-//        }
-//    }
-//
+    @GetMapping("/{id}")
+    public ResponseEntity<TelevisionDto> getTelevisionById(@PathVariable Long id) {
+
+        return ResponseEntity.ok(service.getTelevisionById(id));
+    }
+
     @PostMapping
     public ResponseEntity<TelevisionDto> createTelevision(@Valid @RequestBody TelevisionInputDto televisionInputDto) {
         TelevisionDto televisionDto = this.service.createTelevision(televisionInputDto);
 
         return new ResponseEntity<>(televisionDto, HttpStatus.CREATED);
     }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Television> updateTelevision(@PathVariable Long id, @RequestBody Television television) {
-//        Optional<Television> ot = this.repos.findById(id);
-//        if (ot.isPresent()) {
-//            Television existingTelevision = ot.get();
-//
-//            existingTelevision.setType(television.getType());
-//            existingTelevision.setBrand(television.getBrand());
-//            existingTelevision.setName(television.getName());
-//            existingTelevision.setPrice(television.getPrice());
-//            existingTelevision.setAvailableSize(television.getAvailableSize());
-//            existingTelevision.setRefreshRate(television.getRefreshRate());
-//            existingTelevision.setScreenType(television.getScreenType());
-//            existingTelevision.setScreenQuality(television.getScreenQuality());
-//            existingTelevision.setSmartTv(television.getSmartTv());
-//            existingTelevision.setWifi(television.getWifi());
-//            existingTelevision.setVoiceControl(television.getVoiceControl());
-//            existingTelevision.setHdr(television.getHdr());
-//            existingTelevision.setBluetooth(television.getBluetooth());
-//            existingTelevision.setAmbiLight(television.getAmbiLight());
-//            existingTelevision.setOriginalStock(television.getOriginalStock());
-//            existingTelevision.setSold(television.getSold());
-//            existingTelevision.setLastSold(television.getLastSold());
-//            existingTelevision.setLastPurchased(television.getLastPurchased());
-//
-//            this.repos.save(existingTelevision);
-//            return ResponseEntity.ok(existingTelevision);
-//        } else {
-//            throw new RecordNotFoundException("Television with id " + id + " not found");
-//        }
-//    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TelevisionDto> updateTelevision(@Valid @PathVariable Long id, @RequestBody TelevisionInputDto televisionInputDto) {
+        TelevisionDto televisionDto = this.service.updateTelevision(id, televisionInputDto);
+
+        return ResponseEntity.ok(televisionDto);
+    }
 //
 //    @DeleteMapping("/{id}")
 //    public ResponseEntity<Television> deleteTelevision(@PathVariable Long id) {
--- src/main/java/com/techiteasy/TechItEasy/services/TelevisionService.java
+++ src/main/java/com/techiteasy/TechItEasy/services/TelevisionService.java
@@ -2,6 +2,7 @@ package com.techiteasy.TechItEasy.services;
 
 import com.techiteasy.TechItEasy.dtos.TelevisionDto;
 import com.techiteasy.TechItEasy.dtos.TelevisionInputDto;
+import com.techiteasy.TechItEasy.exceptions.RecordNotFoundException;
 import com.techiteasy.TechItEasy.mappers.TelevisionMapper;
 import com.techiteasy.TechItEasy.models.Television;
 import com.techiteasy.TechItEasy.repositories.TelevisionRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TelevisionService {
@@ -30,11 +32,41 @@ public class TelevisionService {
         return dtoTelevisions;
     }
 
+    public TelevisionDto getTelevisionById(Long id) {
+        return TelevisionMapper.toDto(
+                televisionRepository.findById(id)
+                        .orElseThrow(() -> new RecordNotFoundException("Television with id " + id + " not found")));
+    }
+
     public TelevisionDto createTelevision(TelevisionInputDto televisionInputDto) {
         Television television = TelevisionMapper.toEntity(televisionInputDto);
 
         Television savedTelevision = televisionRepository.save(television);
 
+        return TelevisionMapper.toDto(savedTelevision);
+    }
+
+    public TelevisionDto updateTelevision(Long id, TelevisionInputDto televisionInputDto) {
+        Television existingTelevision = televisionRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Television with id " + id + " not found"));
+
+        existingTelevision.setType(televisionInputDto.type);
+        existingTelevision.setBrand(televisionInputDto.brand);
+        existingTelevision.setName(televisionInputDto.name);
+        existingTelevision.setPrice(televisionInputDto.price);
+        existingTelevision.setAvailableSize(televisionInputDto.availableSize);
+        existingTelevision.setRefreshRate(televisionInputDto.refreshRate);
+        existingTelevision.setScreenType(televisionInputDto.screenType);
+        existingTelevision.setScreenQuality(televisionInputDto.screenQuality);
+        existingTelevision.setSmartTv(televisionInputDto.smartTv);
+        existingTelevision.setWifi(televisionInputDto.wifi);
+        existingTelevision.setVoiceControl(televisionInputDto.voiceControl);
+        existingTelevision.setHdr(televisionInputDto.hdr);
+        existingTelevision.setBluetooth(televisionInputDto.bluetooth);
+        existingTelevision.setAmbiLight(televisionInputDto.ambiLight);
+        existingTelevision.setOriginalStock(televisionInputDto.originalStock);
+
+        Television savedTelevision = televisionRepository.save(existingTelevision);
         return TelevisionMapper.toDto(savedTelevision);
     }
 
