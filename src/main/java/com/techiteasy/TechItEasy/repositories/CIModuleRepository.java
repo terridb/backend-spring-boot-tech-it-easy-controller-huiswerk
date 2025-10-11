be2a31778a89d198836b8308130d2fa2c1dfb4cc
terridb <terrideboer99@gmail.com>
2025-10-11 18:30:19 +0200

    Repositories aangemaakt voor accessoires

---
+++ src/main/java/com/techiteasy/TechItEasy/repositories/CIModuleRepository.java
@@ -0,0 +1,7 @@
+package com.techiteasy.TechItEasy.repositories;
+
+import com.techiteasy.TechItEasy.models.CIModule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CIModuleRepository extends JpaRepository<CIModule, Long> {
+}
