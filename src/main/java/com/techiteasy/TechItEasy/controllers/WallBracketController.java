267f4f1932275ff1b227d120428224b3ebb912c1
terridb <terrideboer99@gmail.com>
2025-10-12 12:54:48 +0200

    Mappers aangemaakt voor accessoires

--- src/main/java/com/techiteasy/TechItEasy/controllers/WallBracketController.java
+++ src/main/java/com/techiteasy/TechItEasy/controllers/WallBracketController.java
@@ -1,8 +1,14 @@
 package com.techiteasy.TechItEasy.controllers;
 
+import com.techiteasy.TechItEasy.dtos.WallBracketDto;
 import com.techiteasy.TechItEasy.services.WallBracketService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("wall-brackets")
@@ -12,5 +18,16 @@ public class WallBracketController {
 
     public WallBracketController(WallBracketService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WallBracketDto>> getAllWallBrackets() {
+
+        return ResponseEntity.ok(this.service.getAllWallBrackets());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WallBracketDto> getWallBracketById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getWallBracketById(id));
     }
 }
---
+++ src/main/java/com/techiteasy/TechItEasy/mappers/WallBracketMapper.java
@@ -0,0 +1,33 @@
+package com.techiteasy.TechItEasy.mappers;
+
+import com.techiteasy.TechItEasy.dtos.WallBracketDto;
+import com.techiteasy.TechItEasy.dtos.WallBracketInputDto;
+import com.techiteasy.TechItEasy.models.WallBracket;
+
+public class WallBracketMapper {
+
+    public static WallBracket toEntity(WallBracketInputDto wallBracketInputDto, WallBracket existingWallBracket) {
+        if (existingWallBracket == null) {
+            existingWallBracket = new WallBracket();
+        }
+
+        existingWallBracket.setSize(wallBracketInputDto.size);
+        existingWallBracket.setAdjustable(wallBracketInputDto.adjustable);
+        existingWallBracket.setName(wallBracketInputDto.name);
+        existingWallBracket.setPrice(wallBracketInputDto.price);
+
+        return existingWallBracket;
+    }
+
+    public static WallBracketDto toDto(WallBracket wallBracket) {
+        WallBracketDto wallBracketDto = new WallBracketDto();
+
+        wallBracketDto.id = wallBracket.getId();
+        wallBracketDto.size = wallBracket.getSize();
+        wallBracketDto.adjustable = wallBracket.getAdjustable();
+        wallBracketDto.name = wallBracket.getName();
+        wallBracketDto.price = wallBracket.getPrice();
+
+        return wallBracketDto;
+    }
+}
